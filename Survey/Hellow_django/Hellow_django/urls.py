514b18bba0caf297a18dd90f7cdff9504a7e423f
Hilal5353 <hilalahmadking53@gmail.com>
2024-12-27 08:21:21 -0800

    My servey project

--- Survey/Hellow_django/Hellow_django/urls.py
+++ Survey/Hellow_django/Hellow_django/urls.py
@@ -6,6 +6,6 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('feedback/<str:id>/', views.customer_feedback, name='feedback'),
+    path('survey/<str:id>/', views.customer_feedback, name='feedback'),
     path('thankyou/', views.thankyou, name='thankyou'),
 ]
